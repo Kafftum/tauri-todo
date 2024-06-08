18fbcef945b145aaf07982a687e97be97fde1a38
CowDev <lildemondsuxx@gmail.com>
2024-06-08 11:12:08 +0200

    Storing in shared state and updating shared state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,44 @@
+use std::sync::Mutex;
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+struct MW {
+  todos: Mutex<Vec<String>>
+}
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![my_first_command])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+      .manage(MW{ todos: Mutex::new(vec![])})
+      .invoke_handler(tauri::generate_handler![store_in_vec, return_vec, remove_todo])
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn my_first_command() -> String {
-    "first".to_string()
+fn store_in_vec(state: tauri::State<MW>, todo: String) {
+  state.todos.lock().unwrap().push(todo);
+}
+
+#[tauri::command]
+fn return_vec(state: tauri::State<MW>) -> Vec<String> {
+  state.todos.lock().unwrap().clone()
+}
+
+#[tauri::command]
+fn remove_todo(state: tauri::State<MW>, todo: String) {
+  let mut todos = state.todos.lock().unwrap();
+  let mut i: usize = 0;
+  loop {
+    if i > todos.len() {
+      break;
+    }
+
+    if todos[i] == todo {
+      todos.remove(i);
+      break;
+    }
+
+    i += 1;
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
 const invoke = window.__TAURI__.invoke;
 
-let todo_display = document.getElementById("todo_display");
+let todo_input = $("#todo_input");
+let add_button = $("#add_button");
+let todo_display = $("#todo_display");
+let todo_list;
 
-document.getElementById("todo_button").addEventListener("click", async () => {
-const result = await invoke("my_first_command");
-
-todo_display.textContent = result;
+add_button.click(async () => {
+  await invoke("store_in_vec", { todo: todo_input.val() });
+  await invoke("return_vec").then((message) => {
+    todo_display.empty();
+    
+    let i;
+    for(i = 0; i < message.length; i++) {
+      createTodo(message[i]);
+    }
+  });
 });
 
+function createTodo(title) {
+  let todo = $("<div>").html(`<h3 class="todo_text">${title}</h3>
+                              <button class="remove_todo">
+                                Remove
+                              </button>`);
+
+  todo.addClass("todo_block");
+  todo.find(".remove_todo").click(async (e) => {
+    e.preventDefault();
+    await invoke("remove_todo", { todo: title });
+    todo.remove();
+  });
+  todo_display.append(todo);
+}
